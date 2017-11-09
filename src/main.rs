b59ed434ec9a8f231e23deffe1635c1639971c7f
octalsrc <nicklewchenko92@gmail.com>
2017-11-09 15:11:47 -0700

    Use getopts for argument parsing, add -i option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate getopts;
+use getopts::Options;
+
 extern crate yaml_rust;
 use yaml_rust::{Yaml, YamlLoader};
 
@@ -12,59 +15,79 @@ use std::io::prelude::*;
 const DB_DEFAULT: &str = "fortunelike-db";
 const DB_VAR: &str = "FORTUNELIKE_DB";
 
-fn main() {
-    let config_file = decide_db_path();
-    match load_config(config_file) {
-        Ok(prompts) => {
-            let mut rng = rand::thread_rng();
-            let key = Range::new(0,prompts.len()).ind_sample(&mut rng);
-            println!("{}",prompts[key]);
-        }
-        Err(_) => println!("{}","[?]")
-    }
+fn user_db_default() -> Option<PathBuf> {
+    std::env::var("HOME").ok()
+        .map(|home: String|
+             [home.as_str(), ".config", DB_DEFAULT]
+             .iter()
+             .collect())
 }
 
-fn decide_db_path() -> PathBuf {
-    let mut path = ["etc", DB_DEFAULT].iter().collect();
-    match std::env::var("HOME") {
-        Ok(home) => path = [&home, ".config", DB_DEFAULT].iter().collect(),
-        Err(_) => ()
-    }
-    match std::env::var(DB_VAR) {
-        Ok(custom) => {
-            path = PathBuf::new();
-            path.push(custom)
-        },
-        Err(_) => ()
-    }
-    
-    let mut path_flag = false;
-    for a in std::env::args() {
-        match path_flag {
-            true => {
-                path_flag = false;
-                path = PathBuf::new();
-                path.push(a)
-            },
-            false => {
-                match Some(&*a) {
-                    Some("-f") => path_flag = true,
-                    Some("--dbfile") => path_flag = true,
-                    _ => ()
-                }
-            }
-        }
-    }
-
-    path
+fn sys_db_default() -> PathBuf {
+    ["/", "etc", DB_DEFAULT].iter().collect()
 }
 
-fn load_config(path: PathBuf) -> std::io::Result<Vec<String>> {
-    let mut config_file = File::open(path)?;
+fn get_file(path: PathBuf) -> Option<File> {
+    File::open(path).ok()
+}
+
+fn choose_config(cmd_opt: Option<String>) -> Option<File> {
+    let sysdb: Option<File> = get_file(sys_db_default());
+    let userdb: Option<File> = user_db_default().and_then(get_file);
+    let envdb: Option<File> =
+        std::env::var(DB_VAR).ok()
+        .map(PathBuf::from)
+        .and_then(get_file);
+    let cmddb: Option<File> =
+        cmd_opt
+        .map(PathBuf::from)
+        .and_then(get_file);
+    cmddb.or(envdb.or(userdb.or(sysdb)))
+}
+
+fn read_config(mut file: File) -> std::io::Result<Vec<String>> {
     let mut config_contents = String::new();
-    config_file.read_to_string(&mut config_contents)?;
+    file.read_to_string(&mut config_contents)?;
     let config_yaml = parse_yaml(&config_contents)?;
     get_values(config_yaml)
+}    
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("f", "dbfile", "set database file path", "DB_FILE");
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("i", "inline", "don't add tailing newline");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let dbfile = matches.opt_str("f");
+    
+    match choose_config(dbfile).and_then(|f| read_config(f).ok()) {
+        Some(prompts) => {
+            let mut rng = rand::thread_rng();
+            let key = Range::new(0,prompts.len()).ind_sample(&mut rng);
+            if matches.opt_present("i") {
+                print!("{}",prompts[key])
+            } else {
+                println!("{}",prompts[key])
+            }
+        }
+        None => println!("{}","[?]")
+    }
 }
 
 fn parse_yaml(path: &str) -> std::io::Result<Yaml> {
