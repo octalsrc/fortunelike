3cf45e2eb59213c1de3b4f00dc94bc28c83ceb33
octalsrc <nicklewchenko92@gmail.com>
2024-09-21 15:51:53 -0600

    Update panic to use format string

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn main() {
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Err(f) => { panic!("{}", f.to_string()) }
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
